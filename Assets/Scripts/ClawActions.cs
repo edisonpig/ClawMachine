4f441a6e3587817690acc439c769e1863c9fd93c
edisonpig <potsunchu@gmail.com>
2024-06-20 16:19:44 +0100

    Initial commit

---
+++ Assets/Scripts/ClawActions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawActions : MonoBehaviour
+{
+    public GameObject clawBox = null;  //for left right x +/-
+    public GameObject clawPipes = null; // for front back z +/-
+    public GameObject clawCore = null; // for up down y +/-
+
+    public GameObject clawA = null; //claw1 rotate x& y =ve
+    public GameObject clawB = null; //claw2 rotate x +ve
+    public GameObject clawC = null; //claw3 rotate x -ve & y +ve
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
